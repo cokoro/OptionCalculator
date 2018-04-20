4ff3f663b03eb016a24a90dd0af545a9faab78f7
陈可心 <cokoro@foxmail.com>
2018-04-20 18:22:36 +0800

    Update algorithm

--- calc/views.py
+++ calc/views.py
@@ -6,69 +6,102 @@ import numpy as np
 from scipy.stats import norm
 import scipy
 from math import sqrt,exp,log
+import math
+from scipy import stats
 
-def geo_asian_option(S0,sigma,r,T,K,option_type):
-    dt = 1e-2
-    N = int(T / dt)
+def arith_asian_option(S,sigma,r,T,E,N,Type,M,flag):
+    Dt = T/N #Dt = 3/50=0.06
+    n = N
+
+    sigsqT = np.square(sigma)*T*(N+1)*(2*N+1)/(6*N*N)
+    muT = 0.5*sigsqT+(r-0.5*np.square(sigma))*T*(N+1)/(2*N)
     
-    sigmaHat = sigma * sqrt((N+1)*(2*N+1)/(6*pow(N,2)))
-    muHat = (r-0.5*pow(sigma, 2))*(N+1)/(2*N)+0.5*pow(sigmaHat, 2)
+    d1 = (math.log(S/E)+(muT+0.5*sigsqT))/(math.sqrt(sigsqT))
+    d2 = d1 - math.sqrt(sigsqT)
     
-    d1 = ( log( S0 / K )+ (muHat + 0.5 * pow(sigmaHat,2)) * (T)) / (sigmaHat * sqrt(T))
-    d2 = d1 - sigmaHat*sqrt(T)
+    drift = math.exp((r-0.5*np.square(sigma))*Dt)    
+    
 
-    if option_type == 'C':
-        return round(exp(-r*(T))*(S0 * exp(muHat*(T))*norm.cdf(d1) - K*norm.cdf(d2)),4)
-    elif option_type == 'P':
-        return round(exp(-r*(T))*(-S0 * exp(muHat*(T))*norm.cdf(-d1) + K*norm.cdf(-d2)),4)
+    if Type == "C":
+        Spath = [0]*N
+        arithPayoff = [0]*M
+        geoPayoff = [0]*M
+        #M unmber of samples
+        geo = math.exp(-r*T)*(S*math.exp(muT)*stats.norm.cdf(d1)-E*stats.norm.cdf(d2)) #closed-form value
+        for i in range(M):
+            Spath = [0]* N
+            growthFactor = drift*math.exp(sigma*math.sqrt(Dt)*np.random.normal())
+            Spath[0] = S * growthFactor
+            for j in range(1,N):
+                growthFactor = drift*math.exp(sigma*math.sqrt(Dt)*np.random.normal())
+                Spath[j] = Spath[j-1] * growthFactor
+        
+            #arithmetic mean
+            arithMean = np.mean(Spath)
+            arithPayoff[i] = math.exp(-r*T)*max(arithMean-E,0)  #payoffs
 
-def browianMotion(S0, dt, c1, c2, random):
-    return S0 * np.exp(c1 * dt + c2 * random)
-
-
-def arith_asian_option(S0,sigma,r,T,K,step,option_type,path,flag):
-    dt = 1e-2
-    N = int(T / dt)
-    drift = exp((r - 0.5 * sigma * sigma) * dt)
-    arithPayOff = np.empty(path, dtype=float)
-    geoPayoff = np.empty(path, dtype=float)
-    scipy.random.seed(5)
-    for i in range(0,path):
-        Rand = scipy.random.randn(N-1)
-        Spath = np.zeros(N)
-        Spath[0] = S0
-        for j in range(1,N):
-            growthFactor = drift * exp(sigma * sqrt(dt) * Rand[j-1])
-            Spath[j] = Spath[j - 1] * growthFactor
-        arithMean = np.mean(Spath)
-        for k in range(0,N):
-            Spath[k] = log(Spath[k])
-        geoMean = exp((1 / N) * sum(Spath))
-
-        if (option_type == "C"):
-            arithPayOff[i] = exp(-r * T) * max(arithMean - K, 0)
-            geoPayoff[i] = exp(-r * T) * max(geoMean - K, 0)
+            
+            #geometric mean
+            SpathLog = [0]*N
+            for n in range(len(Spath)):
+                SpathLog[n]=math.log(Spath[n])
+                
+            geoMean = math.exp((1/N)*np.sum(SpathLog))
+            geoPayoff[i] = math.exp(-r*T)*max(geoMean-E,0)
+                
+   
+        if(flag =="Yes"):
+			Pmean = np.mean(arithPayoff)
+			Pstd = np.std(arithPayoff)
+			confmc = [Pmean-1.96*Pstd/math.sqrt(M), Pmean+1.96*Pstd/math.sqrt(M)] 
+			return round(Pmean,4) 
         else:
-            arithPayOff[i] = exp(-r * T) * max(K - arithMean, 0)
-            geoPayoff[i] = exp(-r * T) * max(K - geoMean, 0)
+        	covXY = np.mean(np.multiply(arithPayoff, geoPayoff))-np.mean(arithPayoff)*np.mean(geoPayoff)
+        	theta = covXY/np.var(geoPayoff)
+        	Z = np.array(arithPayoff) + theta * (geo - np.array(geoPayoff))
+        	Zmean = np.mean(Z)
+        	Zstd = np.std(Z)
+        	confcv = [Zmean-1.96*Zstd/math.sqrt(M), Zmean+1.96*Zstd/math.sqrt(M)]
+        	return round(Zmean,4)
+    else:
+        Spath = [0]*N
+        arithPayoff = [0]*M
+        geoPayoff = [0]*M
+        geo = math.exp(-r*T)*(E*stats.norm.cdf(-d2)-S*math.exp(muT)*stats.norm.cdf(-d1)) #closed-form value
+        for i in range(M):
+            Spath = [0]* N
+            growthFactor = drift*math.exp(sigma*math.sqrt(Dt)*np.random.normal())
+            Spath[0] = S * growthFactor
+            for j in range(1,N):
+                growthFactor = drift*math.exp(sigma*math.sqrt(Dt)*np.random.normal())
+                Spath[j] = Spath[j-1] * growthFactor
+        
+            #arithmetic mean
+            arithMean = np.mean(Spath)
+            arithPayoff[i] = math.exp(-r*T)*max(E-arithMean,0)  #payoffs
 
-    #Standard Monte Carlo
-    if flag == 'No':
-        pmean=np.mean(arithPayOff)
-        pstd=np.std(arithPayOff)
-        confcv=[pmean-1.96*pstd/np.sqrt(path),pmean+1.96*pstd/np.sqrt(path)]
-        return round(np.mean(confcv),4)
-
-    #Control variates
-    elif flag=='Yes':
-        covXY = np.mean(arithPayOff * geoPayoff) - np.mean(arithPayOff) * np.mean(geoPayoff)
-        theta = covXY / np.std(geoPayoff)
-        geo = geo_asian_option(S0, sigma, r, T, K, option_type)
-        Z = arithPayOff + theta * (geo - geoPayoff)
-        Zmean = np.mean(Z)
-        Zstd = np.std(Z)
-        confcv = [Zmean - 1.96 * Zstd / sqrt(path), Zmean + 1.96 * Zstd / sqrt(path)]
-        return round(np.mean(confcv), 4)
+            
+            #geometric mean
+            SpathLog = [0]*N
+            for n in range(len(Spath)):
+                SpathLog[n]=math.log(Spath[n])
+                
+            geoMean = math.exp((1/N)*np.sum(SpathLog))
+            geoPayoff[i] = math.exp(-r*T)*max(E-geoMean,0)
+                
+        if(flag =="Yes"):
+            Pmean = np.mean(arithPayoff)
+            Pstd = np.std(arithPayoff)
+            confmc = [Pmean-1.96*Pstd/math.sqrt(M), Pmean+1.96*Pstd/math.sqrt(M)]   
+            return round(Pmean,4)
+        else:
+			covXY = np.mean(np.multiply(arithPayoff, geoPayoff))-np.mean(arithPayoff)*np.mean(geoPayoff)
+			theta = covXY/np.var(geoPayoff)
+			Z = np.array(arithPayoff) + theta * (geo - np.array(geoPayoff))
+			Zmean = np.mean(Z)
+			Zstd = np.std(Z)
+			confcv = [Zmean-1.96*Zstd/math.sqrt(M), Zmean+1.96*Zstd/math.sqrt(M)]
+			return round(Zmean,4)
 
 def index(request):
     return render(request, 'index.html')
@@ -79,5 +112,6 @@ def add(request, S, K, T, sigma, r,call_put,path,step,controlspecify):
     #import pdb; pdb.set_trace()
 
     c = arith_asian_option(int(S), float(sigma), float(r), float(T), float(K), int(step), str(call_put), int(path), str(controlspecify))
+    print "res = ", c
     r = HttpResponse(str(c))
     return r
